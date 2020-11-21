e246bfa2a4acb703a070e930c23ac7515ba94c31
Jerry Qian <jeq004@ucsd.edu>
2020-11-20 18:29:02 -0800

    initial etl test

--- src/data/etl.py
+++ src/data/etl.py
@@ -6,7 +6,7 @@ import os
 def etl(source_dir, out_dir):
     
     if not os.path.exists(source_dir):
-        symlink_dir = "/teams/DSC180A_FA20_A00/b05vpnxray/GoodData/"
+        symlink_dir = "/teams/DSC180A_FA20_A00/b05vpnxray/GoodData"
 
         os.symlink(symlink_dir, source_dir)
     
