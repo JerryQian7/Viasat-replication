2084148cf9e82d3dcfc37e7851ed91efb713e14b
Jerry Qian <jeq004@ucsd.edu>
2020-11-20 18:52:42 -0800

    initial etl test

--- src/data/etl.py
+++ src/data/etl.py
@@ -6,11 +6,11 @@ import os
 def etl(source_dir, out_dir):
     
     if not os.path.exists(source_dir):
+        #need to figure out why python os.symlink didn't work but ln -s worked
         symlink_dir = "/teams/DSC180A_FA20_A00/b05vpnxray/GoodData"
 
         os.symlink(symlink_dir, source_dir)
     
     
-    datafiles = glob.glob(os.path.join(source_dir, "*"))
-    print(glob.glob('data/raw/*'))
+    datafiles = glob.glob('data/raw/GoodData/*')
     print((datafiles))
