00355b0f7fcffdd1703ab65a6afbcfd2b73eaa19
Jerry Qian <jeq004@ucsd.edu>
2020-11-20 18:14:03 -0800

    initial etl test

--- run.py
+++ run.py
@@ -12,48 +12,29 @@ def main(targets):
     if 'collect' in targets:
         with open('config/data-generation-params.json') as f:
             generation_params = json.load(f)
-            src.data.collect.get_data(generation_params)
+            src.data.generate.get_data(generation_params)
         
     if 'data' in targets:
         # Load, clean, and preprocess data. Then store preprocessed data to
         # intermediate directory.
 
-        print('Data target recognized.')
-
         with open('config/data-params.json', 'r') as f:
             data_params = json.load(f)
-        print('Data configuration loaded.')
-        
-        print('Running ETL pipeline.')
+
         src.data.etl(**data_params)
-        print('ETL pipeline complete.')
 
-    if 'features' in targets:
-        # Load preprocessed data, chunk each file into smaller windows of time,
-        # then engineer features and write sets of features and labels to table.
+    # if 'features' in targets:
+    #     with open('config/features-params.json') as f:
+    #         features_params = json.load(f)
 
-        print('Features target recognized.')
+    #     src.features.apply_features(**features_params)
 
-        with open('config/features-params.json') as f:
-            features_params = json.load(f)
-        print('Features configuration loaded.')
+    # if 'train' in targets:
+    #     with open('config/train-params.json') as f:
+    #         train_params = json.load(f)
 
-        print('Engineering features.')
-        src.features.apply_features(**features_params)
-        print('Feature engineering complete.')
+    #     src.models.train(**train_params)
 
-    if 'train' in targets:
-        # Train a model on feature-engineered data and report its ROC AUC.
-
-        print('Train target recognized.')
-
-        with open('config/train-params.json') as f:
-            train_params = json.load(f)
-        print('Train configuration loaded.')
-
-        print('Training model.')
-        src.models.train(**train_params)
-        print('Model training complete.')
         
         
     return
---
+++ src/data/etl.py
@@ -0,0 +1,16 @@
+import src
+import pandas as pd
+import glob
+import os
+
+
+def etl(source_dir, out_dir):
+    
+    if not os.path.exists(source_dir):
+        symlink_dir = "/teams/DSC180A_FA20_A00/b05vpnxray/GoodData/"
+
+        os.symlink(symlink_dir, source_dir)
+    
+    
+    datafiles = glob.glob(os.path.join(source_dir, "*"))
+    print((datafiles))
